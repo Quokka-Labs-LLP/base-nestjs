1a8524f5c2cf277ed9a7bdcf620f22adaa3335b4
Krishan Bhardwaj <krishan.bhardwaj@quokkalabs.com>
2022-11-15 18:38:17 +0530

    initialize base architecture

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { QueryFailedExceptionFilter } from './common/filters/dbexception.filter';
-import { HttpExceptionFilter } from './common/filters/exception.filter';
-import { GlobalExceptionFilter } from './common/filters/globalexception.filter';
-import { ApiResponseInterceptor } from './common/interceptor/response.interceptor';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const swaggerConfig = new DocumentBuilder()
-  .setTitle('API with NestJS')
-  .setDescription('API developed throughout the API with Common-node-ts repo')
-  .setVersion('1.0')
-  .build();
-
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
-
-  app.useGlobalFilters(new GlobalExceptionFilter(), new QueryFailedExceptionFilter(), new HttpExceptionFilter());
-  app.useGlobalInterceptors(new ApiResponseInterceptor());
-
-
-
   await app.listen(3000);
 }
 bootstrap();
