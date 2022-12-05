151fc5d5b84d7757ba04e4652b263aa4071c3987
Krishan Bhardwaj <krishan.bhardwaj@quokkalabs.com>
2022-12-05 18:43:33 +0530

    added api tags for swagger

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Version } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ResponseInterface } from 'src/common/interfaces/Responses/response.interface';
 import { AppService } from './app.service';
 
+@ApiTags(AppController.name)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get('/root')
+  @Get('/home')
   async getHello(): Promise<ResponseInterface> {
     return { message: this.appService.getHello() };
   }
