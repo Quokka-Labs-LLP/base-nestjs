9fdbe86a4ce98d1ccb46c74b2daf047efea7711b
Zubair Nazir <zubair.nazir@quokkalabs.com>
2023-05-30 12:23:07 +0530

    (UPDATE)[WEBE-113]:mongo user module remove

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { MailModule } from '../mail/mail.module';
 import { dotenvConfig } from '@config/dotenv.config';
 import { DatabaseModule } from '../db/database.module';
-import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
